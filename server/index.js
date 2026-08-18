a46dcc1e2d91e99d0fe346cb64618aac3cf6aac9
Onyeakpa Ogochukwu <ogoike080@gmail.com>
2026-08-18 12:27:36 +0100

    Fix CORS to allow production domain

--- server/index.js
+++ server/index.js
@@ -96,7 +96,8 @@ app.use(
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com", "data:"],
         imgSrc: ["'self'", "data:", "blob:"],
         mediaSrc: ["'self'", "data:", "blob:"],
         connectSrc: ["'self'", "ws:", "wss:", "http:", "https:"]
